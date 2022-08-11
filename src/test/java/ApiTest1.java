91c1f0d13e2a17adc683f34692af772d66c010f1
terencemagwenya <terrymagwenya@gmail.com>
2022-08-11 16:12:36 +0200

    first commit

---
+++ src/test/java/ApiTest1.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.*;
+import org.testng.annotations.Test;
+
+public class ApiTest1 {
+
+    @Test
+    void test1(){
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println("Status Code :"+response.getStatusCode());
+        System.out.println("Response :"+response.asString());
+    }
+
+}
