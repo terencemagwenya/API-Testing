09ae4f3b3d7ab72d11d7972e714ffa3404cce6f6
terencemagwenya <terrymagwenya@gmail.com>
2022-08-11 17:48:36 +0200

    pretty print

--- src/test/java/ApiTest1.java
+++ src/test/java/ApiTest1.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.response.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ApiTest1 {
@@ -7,9 +8,14 @@ public class ApiTest1 {
     @Test
     void test1(){
         Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println("Time taken :"+response.getTime());
         System.out.println("Status Code :"+response.getStatusCode());
-        System.out.println("Response :"+response.asString());
+        System.out.println("Hearder :"+response.getContentType());
         System.out.println("Body"+response.getBody().prettyPrint());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+
     }
 
 }
